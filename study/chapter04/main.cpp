89ca649cd444bf560fcf5e6e678820d8037f3769
Niek Melet <niek+github@meletstudios.com>
2023-12-15 21:51:19 +0100

    Chapter 4 v1
    Finished paragraph one about vectors. Pretty cool ngl

---
+++ study/chapter04/main.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// Hero's Inventory 2.0
+// Demonstrates vectors
+void Chapter4_1();
+
+int main()
+{
+	Chapter4_1();
+	return 0;
+}
+
+void Chapter4_1()
+{
+	vector<string> inventory;
+	inventory.push_back("sword");
+	inventory.push_back("armor");
+	inventory.push_back("shield");
+
+	cout << "You have " << inventory.size() << " items.\n";
+
+	cout << "\nYour items:\n";
+	for(unsigned int i = 0; i < inventory.size(); ++i)
+	{
+		cout << inventory[i] << endl;
+	}
+
+	cout << "\nYou trade your sword for a battle axe.";
+	inventory[0] = "battle axe";
+
+	cout << "\nYour items:\n";
+	for (unsigned int i = 0; i < inventory.size(); ++i)
+	{
+		cout << inventory[i] << endl;
+	}
+
+	cout << "\nThe item name '" << inventory[0] << "' has ";
+	cout << inventory[0].size() << " letters in it.\n";
+
+	cout << "\nYour shield is destroyed in a fierce battle.";
+	inventory.pop_back();
+	cout << "\nYour items:\n";
+	for (unsigned int i = 0; i < inventory.size(); ++i)
+	{
+		cout << inventory[i] << endl;
+	}
+
+	cout << "\nYou were robbed of all of your possessions by a thief.";
+	inventory.clear();
+
+	if(inventory.empty())
+	{
+		cout << "\nYou have nothing.\n";
+	}
+	else
+	{
+		cout << "\nYou have at least one item.\n";
+	}
+}
