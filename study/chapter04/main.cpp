30624312f56ce1f307ea21a3610f3c31401c1407
Niek Melet <niek+github@meletstudios.com>
2023-12-16 11:53:37 +0100

    Chapter 4.2

--- study/chapter04/main.cpp
+++ study/chapter04/main.cpp
@@ -8,9 +8,13 @@ using namespace std;
 // Demonstrates vectors
 void Chapter4_1();
 
+// Hero's Inventory 3.0
+// Demonstrates iterators
+void Chapter4_2();
+
 int main()
 {
-	Chapter4_1();
+	Chapter4_2();
 	return 0;
 }
 
@@ -59,5 +63,53 @@ void Chapter4_1()
 	else
 	{
 		cout << "\nYou have at least one item.\n";
+	}
+}
+
+void Chapter4_2()
+{
+	vector<string> inventory;
+	inventory.push_back("sword");
+	inventory.push_back("armor");
+	inventory.push_back("shield");
+
+	vector<string>::iterator myIterator;
+	vector<string>::const_iterator iter;
+
+	cout << "\nYour items:\n";
+	for(iter = inventory.begin(); iter != inventory.end(); ++iter)
+	{
+		cout << *iter << endl;
+	}
+
+	cout << "\nYou trade your sword for a battle axe.";
+	myIterator = inventory.begin();
+	*myIterator = "battle axe";
+	cout << "\nYour items:\n";
+	for (iter = inventory.begin(); iter != inventory.end(); ++iter)
+	{
+		cout << *iter << endl;
+	}
+
+	cout << "\nThe item name '" << *myIterator << "' has ";
+	cout << (*myIterator).size() << " letters in it.\n";
+
+	cout << "\nThe item name '" << *myIterator << "' has ";
+	cout << myIterator->size() << " letters in it.\n";
+
+	cout << "\nYou recover a crossbow from a slain enemy.";
+	inventory.insert(inventory.begin(), "crossbow");
+	cout << "\nYour items:\n";
+	for (iter = inventory.begin(); iter != inventory.end(); ++iter)
+	{
+		cout << *iter << endl;
+	}
+
+	cout << "\nYour armor is destroyed in a fierce battle.";
+	inventory.erase((inventory.begin() + 2));
+	cout << "\nYour items:\n";
+	for (iter = inventory.begin(); iter != inventory.end(); ++iter)
+	{
+		cout << *iter << endl;
 	}
 }
