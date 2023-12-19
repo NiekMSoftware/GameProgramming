02fca6efed5d443f74f886e7da2fae76107457e6
Niek Melet <niek+github@meletstudios.com>
2023-12-19 10:55:30 +0100

    Chapter 4.1
    Learned how to define new functions

--- study/chapter05/main.cpp
+++ study/chapter05/main.cpp
@@ -1,1 +1,20 @@
-﻿
+﻿#include <iostream>
+
+using namespace std;
+
+// Instructions
+// Demonstrates writing new functions
+void Instructions();
+
+int main()
+{
+	Instructions();
+	return 0;
+}
+
+// function definition
+void Instructions()
+{
+	cout << "Welcome to the most fun you've ever had with text!\n\n";
+	cout << "Here's how to play the game...\n";
+}
