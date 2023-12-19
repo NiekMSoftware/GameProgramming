dfea1b7e3dde9050bbed671d52c831aacdcfef71
Niek Melet <niek+github@meletstudios.com>
2023-12-19 11:09:58 +0100

    Chapter 5.2
    Learned about return values and parameters. Also a tiny amount of encapsulation

--- study/chapter05/main.cpp
+++ study/chapter05/main.cpp
@@ -6,9 +6,15 @@ using namespace std;
 // Demonstrates writing new functions
 void Instructions();
 
+// Yes or No
+// Demonstrates return values and parameters
+char askYesNo1();
+char askYesNo2(string question);
+void askQuestions();
+
 int main()
 {
-	Instructions();
+	askQuestions();
 	return 0;
 }
 
@@ -17,4 +23,39 @@ void Instructions()
 {
 	cout << "Welcome to the most fun you've ever had with text!\n\n";
 	cout << "Here's how to play the game...\n";
+}
+
+char askYesNo1()
+{
+	char response1;
+
+	do
+	{
+		cout << "Please enter 'y' or 'n': ";
+		cin >> response1;
+	} while (response1 != 'y' && response1 != 'n');
+
+	return response1;
+}
+
+char askYesNo2(string question)
+{
+	char response2;
+
+	do
+	{
+		cout << question << " (y/n): ";
+		cin >> response2;
+	} while (response2 != 'y' && response2 != 'n');
+
+	return response2;
+}
+
+void askQuestions()
+{
+	char answer1 = askYesNo1();
+	cout << "Thanks for answering: " << answer1 << "\n\n";
+
+	char answer2 = askYesNo2("Do you wish to save the game?");
+	cout << "Thanks for answering: " << answer2 << "\n";
 }
