795e25506061db7aad715d4bdbf54e4683f2899b
Niek Melet <niek+github@meletstudios.com>
2024-01-22 12:54:47 +0100

    Chapter 9 project | Game Lobby

---
+++ study/GameLobby/Lobby.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include <iostream>
+#include <string>
+
+#include "Player.h"
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::ostream;
+
+class Lobby
+{
+	friend ostream& operator<<(ostream& os, const Lobby& aLobby);
+
+public:
+	Lobby();
+	~Lobby();
+
+	void AddPlayer();
+	void RemovePlayer();
+	void Clear();
+
+private:
+	Player* m_pHead;
+};
--- study/GameLobby/main.cpp
+++ study/GameLobby/main.cpp
@@ -1,4 +1,34 @@
-﻿int main()
+﻿#include "Lobby.h"
+
+int main()
 {
+	Lobby lobby;
+	int choice;
+
+	do
+	{
+		cout << lobby;
+		cout << "\nGAME LOBBY\n";
+		cout << "0 - Exit the program.\n";
+		cout << "1 - Add a player to the lobby.\n";
+		cout << "2 - Remove a player from the lobby.\n";
+		cout << "3 - Clear the lobby.\n";
+		cout << endl << "Enter choice: ";
+		cin >> choice;
+
+		switch (choice)
+		{
+			case 0: cout << "Good-bye.\n";
+				break;
+			case 1: lobby.AddPlayer();
+				break;
+			case 2: lobby.RemovePlayer();
+				break;
+			case 3: lobby.Clear();
+				break;
+			default: cout << "That was not a valid choice.\n";
+		}
+	} while (choice != 0);
+
 	return 0;
 }
