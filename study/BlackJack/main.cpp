4621a25fd73660d07d55b5b9fae27803945c18a6
Niek Melet <niek+github@meletstudios.com>
2024-01-22 18:52:33 +0100

    Refactored stuff

--- study/BlackJack/main.cpp
+++ study/BlackJack/main.cpp
@@ -1,17 +1,6 @@
 ﻿#include <iostream>
 
-#include "Creature.h"
-#include "Orc.h"
-#include "lib/Boss.h"
-#include "lib/Enemy.h"
-
-using std::cout;
-
 int main()
 {
-	Creature* pCreature = new Orc();
-	pCreature->Greet();
-	pCreature->DisplayHealth();
-
 	return 0;
 }
